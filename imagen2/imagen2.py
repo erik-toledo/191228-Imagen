5e318fa480494fbbad6f23da34ea0fb764c78a5b
Erik Toledo Trinidad <toledoeriktrinidad99@gmail.com>
2021-11-03 10:24:53 -0600

    Actividad Imagenes

---
+++ imagen2/imagen2.py
@@ -0,0 +1,42 @@
+from imgurpython import ImgurClient
+ 
+import os
+from multiprocessing import Pool
+import urllib.request
+import timeit
+
+
+secreto_cliente = "5f8c3cce299db5e26a2eb96b0b7809a82805c9ad"
+id_cliente = "bfa0e227a1c5643"
+ 
+cliente = ImgurClient(id_cliente, secreto_cliente)
+ 
+# Metodo para la descarga url imagen
+# Datos necesarios del metodo
+# Nombre de la imagen => yntdWAr
+# Formato de la imagen => png
+ 
+ 
+def descarga_url_img(link):
+   print(link)
+   # Con esto ya podemos obtener el corte de la url imagen
+   nombre_img = link.split("/")[3]
+   formato_img = nombre_img.split(".")[1]
+   nombre_img = nombre_img.split(".")[0]
+   print(nombre_img, formato_img)
+   url_local = "C:/Users/toled/OneDrive/Imágenes/imagenes{}.{}"
+   #Guardar nne local las imagenes
+   urllib.request.urlretrieve(link, url_local.format(nombre_img, formato_img))
+ 
+ 
+def main():
+   id_album = "bUaCfoz"
+   imagenes = cliente.get_album_images(id_album)
+   proceso = len(imagenes)
+ 
+   with Pool(processes=proceso) as pool:
+      pool.map(descarga_url_img,imagenes)
+ 
+ 
+if __name__ == "__main__":
+   print("Tiempo de descarga {}".format(timeit.Timer(main).timeit(number=1)))
